78fe287b13aad7876ea2bf480f1c56320b2b2fcf
red-devil002 <140808020+red-devil002@users.noreply.github.com>
2023-07-30 20:57:56 +0530

    index.js

--- index.js
+++ index.js
@@ -1,15 +1,69 @@
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
+// Function to handle form submission
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
     event.preventDefault();
+
+    // Get form values
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    const dob = document.getElementById("dob").value;
+    const dob = new Date(document.getElementById("dob").value);
     const acceptedTerms = document.getElementById("acceptedTerms").checked;
 
-    const tableBody = document.querySelector("#userTable tbody");
-    const newRow = tableBody.insertRow();
-    newRow.innerHTML = `<td>${name}</td><td>${email}</td><td>${password}</td><td>${dob}</td><td>${acceptedTerms ? 'Yes' : 'No'}</td>`;
+    // Validate age between 18 and 55
+    const age = new Date().getFullYear() - dob.getFullYear();
+    if (age < 18 || age > 55) {
+        alert("You must be between 18 and 55 years old to register.");
+        return;
+    }
 
-    // Clear the form
-    document.getElementById("registrationForm").reset();
+    // Create a new table row for the entry
+    const tableBody = document.getElementById("userTable");
+    const newRow = tableBody.insertRow();
+
+    // Add data to the row
+    const nameCell = newRow.insertCell();
+    const emailCell = newRow.insertCell();
+    const passwordCell = newRow.insertCell();
+    const dobCell = newRow.insertCell();
+    const acceptedTermsCell = newRow.insertCell();
+
+    nameCell.textContent = name;
+    emailCell.textContent = email;
+    passwordCell.textContent = password;
+    dobCell.textContent = dob.toDateString();
+    acceptedTermsCell.textContent = acceptedTerms ? "Yes" : "No";
+
+    
+
+    // Clear the form fields after submission
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("dob").value = "";
+    document.getElementById("acceptedTerms").checked = false;
+
+    // Save the data to localStorage
+    const entryData = JSON.parse(localStorage.getItem('entries')) || [];
+    entryData.push({ name, email });
+    localStorage.setItem('entries', JSON.stringify(entryData));
 });
+
+ // Function to load existing entries from localStorage
+ function loadEntries() {
+    const entryData = JSON.parse(localStorage.getItem('entries')) || [];
+    for (const entry of entryData) {
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `<td>${entry.name}</td><td>${entry.email}</td>`;
+        entryTableBody.appendChild(newRow);
+    }
+}
+
+// Event listener for form submission
+registrationForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    updateTable();
+    registrationForm.reset();
+});
+
+// Load existing entries on page load
+loadEntries();
